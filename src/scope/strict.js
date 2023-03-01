f927307e56a4b32f5774241ef365d8e586085c84
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2023-03-01 12:22:44 -0500

    modo estricto js

---
+++ src/scope/strict.js
@@ -0,0 +1,10 @@
+//modo estricto
+'use strict' //hay que declarar y asignar el valor de la variable
+pi = 3.1416;
+console.log(pi);
+
+function myFunct() {
+    'use strict';
+    return pi;
+}
+myFunct();
