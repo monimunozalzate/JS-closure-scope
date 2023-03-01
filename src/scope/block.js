c8c6ccc9a113a20695bf96c4ae4b1c534dce202e
monimunozalzate <106264010+monimunozalzate@users.noreply.github.com>
2023-03-01 12:16:15 -0500

    reasignar y redeclarar variables const, let, var

--- src/scope/block.js
+++ src/scope/block.js
@@ -1,8 +1,8 @@
 function fruits() {
     //bloque: casi cualquier cosa que este dentro de {}
     if (true) {
-        var fruit1 = 'apple';
-        let fruit2 = 'kiwi';
-        const fruit3 = 'banana';
+        var fruit1 = 'apple'; //function scooe
+        let fruit2 = 'kiwi'; //block scope
+        const fruit3 = 'banana'; //block scope
     }
 }
---
+++ src/scope/re.js
@@ -0,0 +1,21 @@
+var firstName; //valor undefined
+firstName = 'oscar';
+console.log(firstName)
+
+
+var lastName = 'perez'; //declarar y asignar a la vez
+lastName = 'zapata'; //reasignar
+console.log(lastName);
+
+var secondName = 'munoz'; //declarar/asignar
+var secondName = 'alzate'; //reasignar
+
+//let
+let fruit = 'apple'; //declarar y asignar
+fruit = 'kiwi'; //reasignar
+//let fruit = '..' //no se puede redeclarar con let
+
+//const
+const animal = 'dog'; //declara y asigna
+// animal = 'cat'; //no se puede reasignar con CONsT
+//const animal ='snake' // no se puede reasignar
